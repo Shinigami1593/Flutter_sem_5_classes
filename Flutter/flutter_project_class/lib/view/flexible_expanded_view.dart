a1d0132cbb1b9b08fa5093c1bd09dc00a5adfbf4
ayush <rasaileeayush17@gmail.com>
2025-05-14 13:13:47 +0545

    flex and GestreDetector

--- Flutter/flutter_project_class/lib/view/flexible_expanded_view.dart
+++ Flutter/flutter_project_class/lib/view/flexible_expanded_view.dart
@@ -8,16 +8,30 @@ class FlexibleExpandedView extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            height: 400,
-            width: double.infinity,
-            color: Colors.greenAccent,
+          Expanded(
+            child: GestureDetector(
+              // ignore: avoid_print
+              onTap: () => print('container 1 pressed'),
+              child: Container(
+                // height: 400,
+                alignment: Alignment.center,
+                width: double.infinity,
+                color: Colors.greenAccent,
+                child: Text('Container ', style: TextStyle(fontSize: 30),),
+              ),
+            ),
           ),
           Expanded( // flex.loose + flex.tight ko byproduct
-            child: Container(
-              height: 300,
-              width: double.infinity,
-              color: Colors.amber,
+            child: GestureDetector(
+              // ignore: avoid_print
+              onDoubleTap: () => print('container 2 tapped'),
+              child: Container(
+                // height: 300,
+                alignment: Alignment.center,
+                width: double.infinity,
+                color: Colors.amber,
+                child: Text('Container 2', style: TextStyle(fontSize: 30),),
+              ),
             ),
           ),
         ],
