8e3cb8487af01344a14c7147b1f8cd3f9f5e0882
ayush <rasaileeayush17@gmail.com>
2025-06-02 10:11:55 +0545

    code made easy

--- Flutter/all_cubit/lib/app.dart
+++ Flutter/all_cubit/lib/app.dart
@@ -3,6 +3,7 @@ import 'package:all_cubit/cubit/arithmetic_cubit.dart';
 import 'package:all_cubit/cubit/counter_cubit.dart';
 import 'package:all_cubit/cubit/dashboard_cubit.dart';
 import 'package:all_cubit/cubit/student_cubit.dart';
+import 'package:all_cubit/service_locator/service_locator.dart';
 import 'package:all_cubit/view/dashboard_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,23 +13,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => CounterCubit()),
-        BlocProvider(create: (context) => ArithmeticCubit()),
-        BlocProvider(create: (context) => StudentCubit()),
-        BlocProvider(
-          create: (context) => DashboardCubit(
-            context.read<CounterCubit>(),
-            context.read<ArithmeticCubit>(),
-            context.read<StudentCubit>(),
-          ),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter BLoC',
-        home: DashboardView(),
+    // return MultiBlocProvider(
+    //   providers: [
+    //     BlocProvider(create: (context) => CounterCubit()),
+    //     BlocProvider(create: (context) => ArithmeticCubit()),
+    //     BlocProvider(create: (context) => StudentCubit()),
+    //     BlocProvider(
+    //       create: (context) => DashboardCubit(
+    //         context.read<CounterCubit>(),
+    //         context.read<ArithmeticCubit>(),
+    //         context.read<StudentCubit>(),
+    //       ),
+    //     ),
+    //   ],
+    //   child: MaterialApp(
+    //     debugShowCheckedModeBanner: false,
+    //     title: 'Flutter BLoC',
+    //     home: DashboardView(),
+    //   ),
+    // );
+
+    return MaterialApp(
+      title: 'Flutter BLoc',
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider.value(
+        value: serviceLocator<DashboardCubit>(),
+        child: DashboardView(),
       ),
     );
   }
--- Flutter/all_cubit/lib/main.dart
+++ Flutter/all_cubit/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:all_cubit/app.dart';
+import 'package:all_cubit/service_locator/service_locator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initDependencies();
+  
+  runApp(
+    const MyApp()
+  );
 }
