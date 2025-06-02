9fc00b69a44f70ca2f11cc11bf43a91d551f31cb
ayush <rasaileeayush17@gmail.com>
2025-06-02 09:57:00 +0545

    service locator

---
+++ Flutter/all_cubit/lib/service_locator/service_locator.dart
@@ -0,0 +1,26 @@
+// object creation all done here:
+
+import 'package:all_cubit/cubit/arithmetic_cubit.dart';
+import 'package:all_cubit/cubit/counter_cubit.dart';
+import 'package:all_cubit/cubit/dashboard_cubit.dart';
+import 'package:all_cubit/cubit/student_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void>  initDependencies() async{
+  _initBloc();
+}
+
+void _initBloc() {
+  serviceLocator.registerFactory<CounterCubit>(()=>CounterCubit());
+  serviceLocator.registerFactory<ArithmeticCubit>(()=>ArithmeticCubit());
+  serviceLocator.registerFactory<StudentCubit>(()=>StudentCubit());
+  serviceLocator.registerLazySingleton<DashboardCubit>(
+    () => DashboardCubit(
+      serviceLocator(),
+      serviceLocator(),
+      serviceLocator(),
+    )
+  );
+}
