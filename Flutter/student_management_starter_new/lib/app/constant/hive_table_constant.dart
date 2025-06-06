9998205edeb54e682ccc49acbf5b3bd86df5a199
ayush <rasaileeayush17@gmail.com>
2025-06-06 08:14:44 +0545

    kiran sir ko sab

---
+++ Flutter/student_management_starter_new/lib/app/constant/hive_table_constant.dart
@@ -0,0 +1,12 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int studentTableID = 0;
+  static const String studentBox = 'studentBox';
+
+  static const int batchTableID = 1;
+  static const String batchBox = 'batchBox';
+
+  static const int courseTableID = 2;
+  static const String courseBox = 'courseBox';
+}
---
+++ Flutter/student_management_starter_new/lib/core/network/hive_service.dart
@@ -0,0 +1,39 @@
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:student_management/app/constant/hive_table_constant.dart';
+import 'package:student_management/features/batch/data/model/batch_hive_model.dart';
+import 'package:student_management/features/course/data/model/course_hive_model.dart';
+
+class HiveService {
+  Future<void> init() async{
+    //Initialize the database
+    var directory = await getApplicationDocumentsDirectory();
+    var path = '${directory.path}student_management.db';
+
+    Hive.init(path);
+
+    //Register Adapter
+    Hive.registerAdapter(BatchHiveModelAdapter());
+    Hive.registerAdapter(CourseHiveModelAdapter());
+  }
+
+  Future<void> addBatch(BatchHiveModel batch) async{
+    var box = await Hive.openBox<BatchHiveModel>(HiveTableConstant.batchBox);
+    await box.put(batch.batchId, batch);
+  }
+
+  Future<void> deleteBatch(String id) async{
+    var box = await Hive.openBox<BatchHiveModel>(HiveTableConstant.batchBox);
+    await box.delete(id);
+  }
+
+  Future<void> addCourse(CourseHiveModel course) async{
+    var box = await Hive.openBox<CourseHiveModel>(HiveTableConstant.courseBox);
+    await box.put(course.courseId, course);
+  }
+
+  Future<void> deleteCourse(String id) async{
+    var box = await Hive.openBox<CourseHiveModel>(HiveTableConstant.courseBox);
+    await box.delete(id);
+  }
+}
